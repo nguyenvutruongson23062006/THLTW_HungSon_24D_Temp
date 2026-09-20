c652aa291d959d45fa9b56401b64d0bbceec90fb
NguyenVuTruongSon <sonnguyen23062006@gmail.com>
2026-09-20 15:41:10 +0700

    Cập nhật lại Controller thương hiệu và api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DanhMucController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/danhmuc', [DanhMucController::class, 'index']);
+Route::get('/danhmuc/{category}', [DanhMucController::class, 'show']);
