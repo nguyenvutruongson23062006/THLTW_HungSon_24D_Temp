6974cdb0a0db89e443a800cf912e7d94ee5c7234
NguyenVuTruongSon <sonnguyen23062006@gmail.com>
2026-09-20 15:51:35 +0700

    Sửa lại thương hiệu controller và update api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DanhMucController;
+use App\Http\Controllers\ThuongHieuController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,6 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/danhmuc', [DanhMucController::class, 'index']);
 Route::get('/danhmuc/{category}', [DanhMucController::class, 'show']);
+
+Route::get('/thuonghieu', [ThuongHieuController::class, 'index']);
+Route::get('/thuonghieu/{thuongHieu}', [ThuongHieuController::class, 'show']);
